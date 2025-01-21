3371da4cdfe47ce76e7a8b469fa9bff7fa556717
Yoav David <yoavv.david2@gmail.com>
2025-01-22 00:21:49 +0200

    Modified folders structure & Ran prettier

--- src/comments/model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const commentSchema = new mongoose.Schema({
-    postId: { type: Number, required: true },
-    author: { type: String, required: true },
-    content: { type: String, required: true },
-});
-
-export const Comment = mongoose.model('Comment', commentSchema);
