47f3afc6b443687d919d2fc7c916b4f75d36981f
Ori Eluk <ori42410@gmail.com>
2025-01-21 19:45:01 +0200

    feat: added a swagger documentations for the comments routes

--- src/comments/router.js
+++ src/comments/router.js
@@ -3,17 +3,116 @@ const router = express.Router();
 const { createComment, getComments, getCommentsByPost, getCommentById, updateComment, deleteComment } = require('./comments/controller');
 const { authenticateToken } = require('../middlewares/auth');
 
-
+/**
+ * @swagger
+ * /comments:
+ *   post:
+ *     summary: Create a new comment
+ *     tags: [Comments]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Comment'
+ *     responses:
+ *       201:
+ *         description: Comment created successfully
+ */
 router.post('/comments', authenticateToken, createComment);
 
+/**
+ * @swagger
+ * /comments:
+ *   get:
+ *     summary: Get all comments
+ *     tags: [Comments]
+ *     responses:
+ *       200:
+ *         description: List of all comments
+ */
 router.get('/comments', authenticateToken, getComments);
 
+/**
+ * @swagger
+ * /comments/post/{postId}:
+ *   get:
+ *     summary: Get comments by post ID
+ *     tags: [Comments]
+ *     parameters:
+ *       - in: path
+ *         name: postId
+ *         schema:
+ *           type: number
+ *         required: true
+ *         description: ID of the post
+ *     responses:
+ *       200:
+ *         description: Comments for the specified post
+ */
 router.get('/comments/post/:postId', authenticateToken, getCommentsByPost);
 
+/**
+ * @swagger
+ * /comments/{id}:
+ *   get:
+ *     summary: Get a comment by ID
+ *     tags: [Comments]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID of the comment
+ *     responses:
+ *       200:
+ *         description: Comment retrieved successfully
+ */
 router.get('/comments/:id', authenticateToken, getCommentById);
 
+/**
+ * @swagger
+ * /comments/{id}:
+ *   put:
+ *     summary: Update a comment by ID
+ *     tags: [Comments]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID of the comment
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Comment'
+ *     responses:
+ *       200:
+ *         description: Comment updated successfully
+ */
 router.put('/comments/:id', authenticateToken, updateComment);
 
+/**
+ * @swagger
+ * /comments/{id}:
+ *   delete:
+ *     summary: Delete a comment by ID
+ *     tags: [Comments]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID of the comment
+ *     responses:
+ *       200:
+ *         description: Comment deleted successfully
+ */
 router.delete('/comments/:id', authenticateToken, deleteComment);
 
 module.exports = router;
