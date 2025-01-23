39a6c5cd9e1949b576a2a6241a10efadbe470e83
Yoav David <yoavv.david2@gmail.com>
2025-01-23 21:24:20 +0200

    Updated comments model

--- src/models/comments_model.js
+++ src/models/comments_model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema({
-  postId: { type: Number, required: true },
+  postId: { type: String, required: true },
   author: { type: String, required: true },
   content: { type: String, required: true },
 });
